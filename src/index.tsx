6808c47764da0178ffa81c8d6e6f8319c4577d55
nathanclairmonte <nathanclairmonte@outlook.com>
2022-03-06 18:48:45 -0500

    added react navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,11 @@
 import React, { ReactElement } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { AmikoText, AppBootstrap } from "@components";
+import { AppBootstrap } from "@components";
+import Navigator from "@config/Navigator";
 
 export default function App(): ReactElement {
     return (
         <AppBootstrap>
-            <View style={styles.container}>
-                <AmikoText
-                    onPress={() => {
-                        alert(true);
-                    }}
-                    style={{ fontSize: 25 }}
-                    weight="400"
-                >
-                    SLATTTTTTTTTTTT
-                </AmikoText>
-            </View>
+            <Navigator />
         </AppBootstrap>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-    }
-});
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,11 +1,24 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Button } from "react-native";
+import React, { ReactElement } from "react";
 import styles from "./home.styles";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { StackNavigatorParams } from "@config/Navigator";
+import { AmikoText } from "@components";
 
-export default function Home() {
+type HomeProps = {
+    navigation: NativeStackNavigationProp<StackNavigatorParams, "Home">;
+};
+
+export default function Home({ navigation }: HomeProps): ReactElement {
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
+            <AmikoText style={{ fontSize: 25 }}>Home</AmikoText>
+            <Button
+                title="Game"
+                onPress={() => {
+                    navigation.navigate("Game", { gameId: "test" });
+                }}
+            />
         </View>
     );
 }
