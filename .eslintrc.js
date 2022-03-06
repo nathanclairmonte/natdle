253522b2d7f2bf3a45cb87b572a1468693709119
nathanclairmonte <nathanclairmonte@outlook.com>
2022-03-05 20:09:00 -0500

    setting up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/react"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    "rules": {
+        "@typescript-eslint/no-unused-vars": "off"
+    }
+}
