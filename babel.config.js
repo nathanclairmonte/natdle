4263c5c469441eaec4d77389d9f06b7a13feed58
nathanclairmonte <nathanclairmonte@outlook.com>
2022-03-06 17:01:10 -0500

    setting up folder structure + module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ["babel-preset-expo"]
+        presets: ["babel-preset-expo"],
+        plugins: [
+            [
+                "module-resolver",
+                {
+                    alias: {
+                        "@assets": "./assets",
+                        "@components": "./src/components",
+                        "@config": "./src/config",
+                        "@contexts": "./src/contexts",
+                        "@screens": "./src/screens",
+                        "@utils": "./src/utils"
+                    }
+                }
+            ]
+        ]
     };
 };
