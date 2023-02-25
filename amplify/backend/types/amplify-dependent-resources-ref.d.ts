9b50a9b51ab40ec55b260c10fb25fda98c610d6e
nathanclairmonte <nathanclairmonte@outlook.com>
2023-02-24 19:50:51 -0500

    initialized aws amplify with graphQL

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    api: {
+        natdle: {
+            GraphQLAPIEndpointOutput: "string";
+            GraphQLAPIIdOutput: "string";
+        };
+    };
+    auth: {
+        natdle5fd4a4ba: {
+            AppClientID: "string";
+            AppClientIDWeb: "string";
+            IdentityPoolId: "string";
+            IdentityPoolName: "string";
+            UserPoolArn: "string";
+            UserPoolId: "string";
+            UserPoolName: "string";
+        };
+    };
+};
