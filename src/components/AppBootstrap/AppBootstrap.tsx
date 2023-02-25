4b8c85bcc14cbb1754f3592f194cc32e40c68ba8
nathanclairmonte <nathanclairmonte@outlook.com>
2023-02-24 19:49:00 -0500

    refactored to use new expo SplashScreen

--- src/components/AppBootstrap/AppBootstrap.tsx
+++ src/components/AppBootstrap/AppBootstrap.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
-import React, { ReactNode, ReactElement } from "react";
-import AppLoading from "expo-app-loading";
+import React, { ReactNode, ReactElement, useEffect } from "react";
+// import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
 import {
     useFonts,
     Amiko_400Regular,
@@ -12,6 +13,19 @@ type AppBootstrapProps = {
     children: ReactNode;
 };
 
+// // old way of doing it (from react-native course)
+// export default function AppBootstrap({ children }: AppBootstrapProps): ReactElement {
+//     const [fontLoaded] = useFonts({
+//         Amiko_400Regular,
+//         Amiko_600SemiBold,
+//         Amiko_700Bold
+//     });
+
+//     return fontLoaded ? <>{children}</> : <AppLoading />;
+// }
+
+SplashScreen.preventAutoHideAsync();
+
 export default function AppBootstrap({ children }: AppBootstrapProps): ReactElement {
     const [fontLoaded] = useFonts({
         Amiko_400Regular,
@@ -19,5 +33,17 @@ export default function AppBootstrap({ children }: AppBootstrapProps): ReactElem
         Amiko_700Bold
     });
 
-    return fontLoaded ? <>{children}</> : <AppLoading />;
+    useEffect(() => {
+        const hideSplashScreen = async () => {
+            if (fontLoaded) {
+                // waiting for 1.571 seconds before removing splashscreen
+                await new Promise((resolve) => setTimeout(resolve, 1571));
+                await SplashScreen.hideAsync();
+            }
+        };
+
+        hideSplashScreen().catch(console.error);
+    }, [fontLoaded]);
+
+    return fontLoaded ? <>{children}</> : <>{null}</>;
 }
