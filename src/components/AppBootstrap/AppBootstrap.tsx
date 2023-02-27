ad995c59721340da9ddd80668371e3ceb39ac20f
nathanclairmonte <nathanclairmonte@outlook.com>
2023-02-26 23:58:24 -0500

    added AuthContext (pre-refactor, in AppBootstrap)

--- src/components/AppBootstrap/AppBootstrap.tsx
+++ src/components/AppBootstrap/AppBootstrap.tsx
@@ -1,5 +1,13 @@
 import { View, Text } from "react-native";
-import React, { ReactNode, ReactElement, useEffect } from "react";
+import React, {
+    ReactNode,
+    ReactElement,
+    useEffect,
+    useState,
+    createContext,
+    Dispatch,
+    SetStateAction
+} from "react";
 // import AppLoading from "expo-app-loading";
 import * as SplashScreen from "expo-splash-screen";
 import {
@@ -8,10 +16,69 @@ import {
     Amiko_600SemiBold,
     Amiko_700Bold
 } from "@expo-google-fonts/amiko";
+import { Auth } from "aws-amplify";
 
 type AppBootstrapProps = {
     children: ReactNode;
 };
+
+type AuthContextType = {
+    user: { [key: string]: any } | null;
+    setUser: Dispatch<SetStateAction<{ [key: string]: any } | null>>;
+};
+
+SplashScreen.preventAutoHideAsync();
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export default function AppBootstrap({ children }: AppBootstrapProps): ReactElement {
+    const [fontLoaded] = useFonts({
+        Amiko_400Regular,
+        Amiko_600SemiBold,
+        Amiko_700Bold
+    });
+    const [authLoaded, setAuthLoaded] = useState(false);
+    const [user, setUser] = useState<{ [key: string]: any } | null>(null);
+
+    useEffect(() => {
+        const checkCurrentUser = async () => {
+            try {
+                const user = await Auth.currentAuthenticatedUser();
+                setUser(user);
+            } catch (error) {
+                // if there's an error just set user back to null (i.e. logout)
+                setUser(null);
+            }
+            setAuthLoaded(true);
+        };
+        checkCurrentUser();
+    }, []);
+
+    useEffect(() => {
+        const hideSplashScreen = async () => {
+            if (fontLoaded && authLoaded) {
+                // waiting for 1.571 seconds before removing splashscreen
+                // await new Promise((resolve) => setTimeout(resolve, 1571));
+                await SplashScreen.hideAsync();
+            }
+        };
+
+        hideSplashScreen().catch(console.error);
+    }, [fontLoaded, authLoaded]);
+
+    return fontLoaded && authLoaded ? (
+        <AuthContext.Provider
+            value={{
+                user: user,
+                setUser: setUser
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    ) : (
+        <>{null}</>
+    );
+}
 
 // // old way of doing it (from react-native course)
 // export default function AppBootstrap({ children }: AppBootstrapProps): ReactElement {
@@ -23,27 +90,3 @@ type AppBootstrapProps = {
 
 //     return fontLoaded ? <>{children}</> : <AppLoading />;
 // }
-
-SplashScreen.preventAutoHideAsync();
-
-export default function AppBootstrap({ children }: AppBootstrapProps): ReactElement {
-    const [fontLoaded] = useFonts({
-        Amiko_400Regular,
-        Amiko_600SemiBold,
-        Amiko_700Bold
-    });
-
-    useEffect(() => {
-        const hideSplashScreen = async () => {
-            if (fontLoaded) {
-                // waiting for 1.571 seconds before removing splashscreen
-                await new Promise((resolve) => setTimeout(resolve, 1571));
-                await SplashScreen.hideAsync();
-            }
-        };
-
-        hideSplashScreen().catch(console.error);
-    }, [fontLoaded]);
-
-    return fontLoaded ? <>{children}</> : <>{null}</>;
-}
