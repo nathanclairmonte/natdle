153ed5469adedc23f0e979e700e4f32ccb8ce9c5
nathanclairmonte <nathanclairmonte@outlook.com>
2023-02-26 23:56:31 -0500

    adding AWS amplify to project

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React, { ReactElement } from "react";
 import { AppBootstrap } from "@components";
 import Navigator from "@config/Navigator";
 import { SettingsProvider } from "@contexts/Settings-context";
+import { Amplify } from "aws-amplify";
+import awsconfig from "../aws-exports";
+
+Amplify.configure(awsconfig);
 
 export default function App(): ReactElement {
     return (
