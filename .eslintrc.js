6ecdadef6f78779c8eca9050852fa963aec7d4dc
nathanclairmonte <nathanclairmonte@outlook.com>
2023-02-26 23:57:43 -0500

    turning off no-explicit-any rule in lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         }
     },
     rules: {
-        "@typescript-eslint/no-unused-vars": "off"
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-explicit-any": "off"
     }
 };
